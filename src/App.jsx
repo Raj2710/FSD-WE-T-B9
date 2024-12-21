69cf34e447facb05d760762909b5314096a3ec43
Raj2710 <nagarajansai2727@gmail.com>
2024-12-21 13:06:08 +0530

    Added Key

--- src/App.jsx
+++ src/App.jsx
@@ -54,20 +54,18 @@ function App() {
       </h1>
       <div className="row">
             {
-              data.map((e)=>{
-                  return <>
-                  <div class="col-lg-4 col-sm-6 mb-4">
-                    <div class="card h-100">
-                      <a href="#"><img class="card-img-top" src={e.image} alt=""/></a>
-                      <div class="card-body">
-                        <h4 class="card-title">
+              data.map((e,index)=>{
+                  return <div className="col-lg-4 col-sm-6 mb-4" key={index}>
+                    <div className="card h-100">
+                      <a href="#"><img className="card-img-top" src={e.image} alt=""/></a>
+                      <div className="card-body">
+                        <h4 className="card-title">
                           <a href="#">{e.title}</a>
                         </h4>
-                        <p class="card-text">{e.description}</p>
+                        <p className="card-text">{e.description}</p>
                       </div>
                     </div>
                   </div>
-                </>
               })
             }
       </div>
