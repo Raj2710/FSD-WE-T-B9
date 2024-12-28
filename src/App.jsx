c4113b3c79a1ce4fa89d2f0609b3e988e2ffbee6
Raj2710 <nagarajansai2727@gmail.com>
2024-12-28 11:33:11 +0530

    Shop App

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Nav from './components/Nav'
+import Card from './components/Card'
+function App() {
+  return <>
+    <Nav/>
+    <section className="py-5">
+            <div className="container px-4 px-lg-5 mt-5">
+                <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    <Card/>  
+                </div>
+            </div>
+    </section>
+  </>
+}
+
+export default App
