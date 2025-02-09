8fc442acb9a7092e08c42b046513efe719d8c0b6
Raj2710 <nagarajansai2727@gmail.com>
2025-02-09 12:50:39 +0530

    Added CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 8000
 const USERS = [
@@ -26,6 +27,7 @@ const USERS = [
 
 //middleware - its a function that will be used within req,res cycle
 app.use(express.json())
+app.use(cors())
 
 app.use((req,res,next)=>{
     console.log(`${req.method} ${req.url}`)
